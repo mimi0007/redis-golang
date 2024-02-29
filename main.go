99ca997f6682a861f9097f8247f79f667087039e
mimi0007 <afsanaaziz.mimi@gmail.com>
2024-02-29 14:47:50 +0600

    Added the connection to the redis instance

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Hello World of redis!")
+
+	redisClient := redis.NewClient(
+		&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "",
+			DB:       0,
+		},
+	)
+
+	ctx := context.Background()
+
+	fmt.Println(redisClient.Ping(ctx)) //this will print ping: PONG if the redis client is connected successfully
 }
